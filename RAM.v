00084b2388354d6237a11fb6eb48e63f1efeb31c
Nathan Daniel <20nmd1@queensu.ca>
2025-03-19 20:30:54 -0400

    fixed the RAM file

--- RAM.v
+++ RAM.v
@@ -9,10 +9,11 @@ module RAM (
 
 	//creating the 512 x 32-bit array in memory
 	reg [31:0] ram [0:511];
+
+	integer i;
 	
 	//Initialize RAM contents to zero
 	initial begin
-        integer i;
         for (i = 0; i < 512; i = i + 1) 
             ram[i] = 32'b0;
     end
