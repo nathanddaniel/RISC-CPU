24a82be6a8fc9ba67bc25e8907467ac67cbaa920
Nathan Daniel <20nmd1@queensu.ca>
2025-03-31 19:13:57 -0400

    added the new hard coded instructions into the RAM

--- RAM.v
+++ RAM.v
@@ -21,21 +21,32 @@ module RAM (
 			
 		// Hardcoded instructions
 		ram[9'h00] = 32'h02000054;  // ld R4, 0x54
-		ram[9'h01] = 32'h0C863000;  // ld R6, 0x63(R2)
-      ram[9'h02] = 32'h14054000;  // ldi R4, 0x54
-      ram[9'h03] = 32'h16263000;  // ldi R6, 0x63(R2)
-      ram[9'h04] = 32'h23034000;  // st 0x34, R3
-      ram[9'h05] = 32'h23334000;  // st 0x34(R3), R3
+		ram[9'h01] = 32'h03100063; // ld R6, 0x63(R2)
+        ram[9'h02] = 32'h0A000054; // ldi R4, 0x54
+        ram[9'h03] = 32'h0B100063; // ldi R6, 0x63(R2)
+        ram[9'h04] = 32'h10180034; // st 0x34, R3
+        ram[9'h05] = 32'h11980034; // st 0x34(R3), R3
+		ram[9'h06] = 32'h98800027; // brzr R1, 27
+		ram[9'h07] = 32'h98880027; // brnz R1, 27
+		ram[9'h08] = 32'h98900027; // brpl R1, 27
+		ram[9'h09] = 32'h98980027; // brmi R1, 27
+		ram[9'h10] = 32'h62B7FFF9; // addi R5, R6, -7
+		ram[9'h11] = 32'h72B00095; // ori R5, R6, 0x95
+		ram[9'h12] = 32'h6AB00095; // andi R5, R6, 0x95
+		ram[9'h13] = 32'hAC000000; // jr R8
+		ram[9'h14] = 32'hA2C00000; // jal R5
+		ram[9'h15] = 32'hC1000000; // mflo R2
+		ram[9'h16] = 32'hC9800000; // mfhi R3
+		ram[9'h17] = 32'hBB000000; // out R6
+		ram[9'h18] = 32'hB1800000; // in R3
+		  
 		  
 		// Hardcoded memory values
 		ram[9'h054] = 32'h00000097;  // Data at RAM[0x54] = 0x97
-      ram[9'h078] = 32'h00000078;  // R2 contains 0x78
-      ram[9'h0DB] = 32'h00000046;  // Data at RAM[0xDB] = 0x46
-      ram[9'h034] = 32'h00000025;  // Data at RAM[0x34] = 0x25 (before store)
-      ram[9'h0EA] = 32'h00000019;  // Data at RAM[0xEA] = 0x19 (before store)
-		  
-
- 
+        ram[9'h078] = 32'h00000078;  // R2 contains 0x78
+        ram[9'h0DB] = 32'h00000046;  // Data at RAM[0xDB] = 0x46
+        ram[9'h034] = 32'h00000025;  // Data at RAM[0x34] = 0x25 (before store)
+        ram[9'h0EA] = 32'h00000019;  // Data at RAM[0xEA] = 0x19 (before store)
     end
 	
 	always @ (Clock) begin
